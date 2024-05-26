908810d033016bc4bc0720cdc91a1e76dbb4dca1
John Warui Waithaka <johnwaruiwaithaka@gmail.com>
2024-05-26 23:15:12 +0300

    adding props in about,home and link

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,32 @@
 import React from "react";
 
-function About() {
+//Warui import links into About
+import Links from "./Links"
+
+
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+    
+      {/* It checks if the props.bio exists and has a length greater than 1. 
+      If both conditions are true, it renders a paragraph (<p>) containing the props.bio content. Otherwise, it renders nothing (null). */}
+
+      {props.bio && props.bio.length > 1 ? <p>{props.bio}</p> : null}
+
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+
+      <Links github={props.links.github} linkedin={props.links.linkedin} />
+
     </div>
   );
 }
 
+
+
 export default About;
+
+
+
+
